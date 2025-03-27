deb607eb9e1bbe0fa26ae9c59a1da984b9a98e68
LostDeveloper-spec <B@Bennettrandall.com>
2025-03-27 15:08:58 -0600

    Add files via upload

---
+++ meal.py
@@ -0,0 +1,17 @@
+def convert(time):
+    hours, minutes = map(int, time.split(":"))
+    return hours + minutes / 60
+
+
+time = input("Enter the time (HH:MM): ").strip()
+hours = convert(time)
+
+if 7.0 <= hours <= 8.0:
+    print("breakfast time")
+elif 12.0 <= hours <= 13.0:
+    print("lunch time")
+elif 18.0 <= hours <= 19.0:
+    print("dinner time")
+
+
+
